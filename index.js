4644fc8fab3dc35411f0e72a527cc71174cf99d8
Ryan Mason <ryanmasonmusic@gmail.com>
2022-11-22 22:20:08 -0500

    commit tree 8862909d2d0d6a98ce151c5409c84554d7a5be03

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() *10) +1;
 
-// Write your code here
+const num3 = 8;
+const num4 = 2;
+const mod = num3 / num4;
+
+const max = Math.max(1, 2, 3, 20);
